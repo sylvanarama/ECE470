f755f523cb33d3fcb92250f67a0e18497abffb9d
Madeleine <mtownley@hotmail.ca>
2018-07-12 16:26:29 -0700

    Implemented single point crossover

--- crossover.m
+++ crossover.m
@@ -1,5 +1,12 @@
 % Crossover operator **UNFINISHED**
 
-function new_data = crossover(data)
-    new_data = data;
+function offspring = crossover(parents, pop_size)
+    offspring = zeros(size(parents));
+    for i = 1:2:pop_size-1
+        cross_pt = randi(90);
+        parent1 = parents(randi(pop_size), :);
+        parent2 = parents(randi(pop_size), :);
+        offspring(i,:) = [parent1(1:cross_pt), parent2(cross_pt+1:end)];
+        offspring(i+1,:) = [parent1(1:cross_pt), parent2(cross_pt+1:end)];
+    end
 end
