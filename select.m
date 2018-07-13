a1cdb2087f0c0e78060efe277a4a0669656dd0df
Madeleine <mtownley@hotmail.ca>
2018-07-12 22:17:48 -0700

    Changed select.m to use datasample (matlab builtin)

--- select.m
+++ select.m
@@ -5,17 +5,17 @@
 
 function new_pop = select(population, fitness)
     pop_size = size(population,1);
-    new_pop = zeros(size(population));
-    wheel = zeros(1,pop_size);
-    for i = 1:pop_size
-        wheel(i) = sum(fitness(1:i));
-    end
-    wheel = wheel/max(wheel);
-    
-    for i = 1:pop_size
-        x = rand();
-        index = find(wheel>=x, 1);
-        new_pop(i,:) = population(index,:);
-    end
-        
+%     new_pop = zeros(size(population));
+%     wheel = zeros(1,pop_size);
+%     for i = 1:pop_size
+%         wheel(i) = sum(fitness(1:i));
+%     end
+%     wheel = wheel/max(wheel);
+%     
+%     for i = 1:pop_size
+%         x = rand();
+%         index = find(wheel>=x, 1);
+%         new_pop(i,:) = population(index,:);
+%     end
+      new_pop = datasample(population, pop_size, 'Weights', fitness);
 end
