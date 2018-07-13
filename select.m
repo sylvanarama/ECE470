89a40f2f25077d013ca303b917eb7b8eef16e1f9
Madeleine <mtownley@hotmail.ca>
2018-07-12 17:27:46 -0700

    Implemented roulette wheel selection

--- select.m
+++ select.m
@@ -1,5 +1,21 @@
-% Selection operator **UNFINISHED**
+% SELECTION OPERATOR
+% Creates a new population by selecting chromosomes with a probability 
+% proportional to their fitness (roulette wheel selection)
+% Fitness: measure of chromosome's classification performance
 
-function new_data = select(population, fitness)
-    new_data = population;
+function new_pop = select(population, fitness)
+    pop_size = size(population,1);
+    new_pop = zeros(size(population));
+    wheel = zeros(1,pop_size);
+    for i = 1:pop_size
+        wheel(i) = sum(fitness(1:i));
+    end
+    wheel = wheel/max(wheel);
+    
+    for i = 1:pop_size
+        x = rand();
+        index = find(wheel>=x, 1);
+        new_pop(i,:) = population(index,:);
+    end
+        
 end
