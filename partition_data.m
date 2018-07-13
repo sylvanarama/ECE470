ef17f6dea22015361c748b826fdc8c48b9af1596
Madeleine <mtownley@hotmail.ca>
2018-07-12 17:55:29 -0700

    Implemented data partioning into training, testing, and validation sets

--- partition_data.m
+++ partition_data.m
@@ -1,7 +1,25 @@
-% Partition data into testing and training sets **UNFINISHED**
+% Partition data into training, testing, and validation sets 
 
-function [train, test, test_labels] = partition_data(features)
-    train = features(1:100, :);
-    test = features(101:200, 2:end);
-    test_labels = features(101:200, 1);
+function [train, test, test_labels, validate] = partition_data(features)
+    
+    % Randomly permute the dataset indices
+    num_samples = size(features, 1);
+    perm = randperm(num_samples);
+
+    % Split the dataset into 70% training and 30% validation
+    split = floor(num_samples * 0.7);
+    train_indices = perm(1:split);
+    validation_indices = perm(split + 1:end);
+
+    % Split the training data into 70% training and 30% testing
+    split = floor(length(train_indices) * 0.7);
+    test_indices = train_indices(split+1:end);
+    train_indices = train_indices(1:split);
+    
+    % Partition the dataset 
+    train = features(train_indices, :);
+    test = features(test_indices, 2:end);
+    test_labels = features(test_indices, 1);
+    validate = features(validation_indices, :);
+
 end
