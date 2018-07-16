e3a3cb2a72014be6bdd37e6ffdc70429f47eac7c
Madeleine <mtownley@hotmail.ca>
2018-07-16 12:07:59 -0700

    Mutation with elitism (don't mutate best chromosomes)

--- mutate.m
+++ mutate.m
@@ -1,18 +1,16 @@
 % Mutation operator: introduce mutations (bit flip) with a probability of p
 
-function mutated_pop = mutate(population, p)
+function mutated_pop = mutate(population, p, ep)
     pop_size = size(population,1);
     num_features = size(population,2);
-    mutated_pop = zeros(size(population));
-    for i = 1:pop_size
+    n = floor((ep*pop_size)/2);
+    for i = n+1:pop_size
         for j = 1:num_features
             x = rand();
             if x <= p
-                mutated_pop(i,j) = ~population(i,j);
-            else
-                mutated_pop(i,j) = population(i,j);
+                population(i,j) = ~population(i,j);
             end
-   
         end
     end
+    mutated_pop = population;
 end
