f77308784d46f20476c450d5f4f07eb851308c0b
Madeleine <mtownley@hotmail.ca>
2018-07-16 12:32:28 -0700

    Optimizations to GA structure
    1. uses smaller dataset for training speed
    2. uses knn as learners for training speed
    3. initialises population with opposition (see notes,txt)
    4. used apaptive crossover/mutation
    5. replaces empty chromosomes (all features 0) with randomly chosen value

--- GA_project.m
+++ GA_project.m
@@ -1,58 +1,85 @@
 % Feature Selection for Year Prediction in Songs from 1922 t0 2011
 
 % Set Parameters
-mp = 0.05; % mutation probability
-feature_num = 90;
-pop_size = 10; % dummy value --> look for good population number
+pop_size = 199; % population size
+ep = 0.1;      % elitism percentage (percentage of fittest chromosomes to preserve)
+cp = 0.93;      % crossover probability (when not using adaptive)
+mp = 0.05;      % mutation probability (when not using adaptive)
+k1 = 1.0;      % Adaptive crossover factor
+k2 = 0.5;      % Adaptive mutation factor
+
+len = 90;
 prev_gen_result = [0, 0, 0];
 delta = [1, 1, 1];
+max_gen = 200;
+results = zeros(max_gen, 3);
 
 % 1. Load data: 1 million songs encoded with 90 audio features each, and
 % partition into training, testing, and validation sets
 %all_songs = readtable("year_prediction.csv");
 %[train_data, test_data, test_labels, validation_data] = partition_data(all_songs);
 [train_data, test_data, test_labels, validation_data] = partition_data(song_subset1000);
+train_data = datasample(train_data, 2500);
+[test_data, index] = datasample(test_data, 500);
+test_labels = test_labels(index);
 
-while(true)
+% 2. Generate chromosome: binary encoding representing the use or exclusion
+% of each feature for classification (100 random binary strings)
+%population = randi([0,1], pop_size, feature_num);
+%population = zeros(pop_size, len);
+n = floor((pop_size-(2*len)-1)/2);
+r = randi([0,1], n, len);
+population = [ones(1,len); eye(len); ~eye(len); r; ~r];
+%population(pop_size/2+1:end, :) = ~population(2:pop_size/2, :);
 
-    % 2. Generate chromosome: binary encoding representing the use or exclusion
-    % of each feature for classification
-    
-    % Generate 100 random binary strings representing feature sets
-    population = randi([0,1], pop_size, feature_num);
+
+for gen = 1:max_gen
+    tic
+    fprintf("Generation %d\n", gen);
 
     % 3. Evaluate the fitness of each chromosome for classification accuracy
-    fitness = zeros(pop_size);
+    fitness = zeros(pop_size, 1);
     for i = 1:pop_size
-        disp(i);
         % find indices of selected features
         k = find(population(i,:)); 
         % create training and testing set using only seected features
-        train_features = train_data(:, [1,k+1]); 
-        test_features = test_data(:, k);
-        tic
-        model = fitcecoc(train_features, 'label');
-        toc
-        tic
-        [predicted_labels] = predict(model, test_data); 
-        toc
-        fitness(i) = calc_fitness(predicted_labels, test_labels);    
+        train_subset = train_data(:, [1,k+1]); 
+        test_subset = test_data(:, k);
+        model = fitcecoc(train_subset, 'label', 'Coding', 'onevsall', 'Learners', 'knn');
+        [predicted_labels] = predict(model, test_subset); 
+        fitness(i) = calc_fitness(predicted_labels, test_labels);
+        %fprintf("Fitness for chromosome %d is %d\n", i, fitness(i));
     end
-
+    
+    %fprintf("Fitnesses: \n");
+    %disp(fitness);
+    
     % 4. Check for termination condition
     [result, terminate] = termination_condition(population, fitness, prev_gen_result, delta);
+    fprintf("D = %5.2f, Mean = %1.2f, Max = %1.2f\n", result(1), result(2), result(3));
+    results(gen, :) = result;
     if(terminate) 
         break;
     end
-
+    prev_gen_result = result;
+    
     % 5. Select members from the population proportionally to their fitness
-        new_pop = select(population, fitness);
+        [new_pop, new_fit] = select(population, fitness, ep);
 
     % 6. Perform crossover on selected chromosomes
-        new_pop = crossover(new_pop);
+        new_pop = crossover_adaptive(new_pop, new_fit, ep, k1, k2);
 
     % 7. Perfrom mutation on selected chromosomes
-        new_pop = mutate(new_pop, mp);
+       % new_pop = mutate(new_pop, mp, ep);
+        
+        population = new_pop;
+        for i = 1:pop_size
+            if population(i,:) == zeros(1,len)
+                population(i,:) = randi([0,1],1,len);
+            end
+        end
+        
+        toc
 end
 
 % 8. Output final solution
