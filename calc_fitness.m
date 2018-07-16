ee7cceb799f842f950fa73cb6e2db7bb5de81e12
Madeleine <mtownley@hotmail.ca>
2018-07-16 12:00:20 -0700

    Fitness calculated based on distance from target year

--- calc_fitness.m
+++ calc_fitness.m
@@ -1,7 +1,7 @@
-% Calculate fitness from predictions **UNFINISHED**
+% Calculate fitness from predictions
 
 function fitness = calc_fitness(predicted_labels, test_labels)
-    test_labels = table2array(test_labels);
-    fitness = (predicted_labels == test_labels);
-    fitness = sum(fitness);
+    fitness =(abs(predicted_labels - test_labels)).^-1;
+    fitness(fitness == Inf) = 1;
+    fitness = sum(fitness)/length(test_labels);
 end
