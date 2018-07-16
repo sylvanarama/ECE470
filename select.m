b950fd584c232310a5bde1fda76c244acccfa3ce
Madeleine <mtownley@hotmail.ca>
2018-07-16 12:35:41 -0700

    Added sorting to selection to improve probability distribution

--- select.m
+++ select.m
@@ -3,19 +3,33 @@
 % proportional to their fitness (roulette wheel selection)
 % Fitness: measure of chromosome's classification performance
 
-function new_pop = select(population, fitness)
+function [new_pop, new_fit] = select(population, fitness, elitism)
     pop_size = size(population,1);
-%     new_pop = zeros(size(population));
-%     wheel = zeros(1,pop_size);
-%     for i = 1:pop_size
-%         wheel(i) = sum(fitness(1:i));
-%     end
-%     wheel = wheel/max(wheel);
-%     
-%     for i = 1:pop_size
-%         x = rand();
-%         index = find(wheel>=x, 1);
-%         new_pop(i,:) = population(index,:);
-%     end
-      new_pop = datasample(population, pop_size, 'Weights', fitness);
+    new_pop = zeros(size(population));
+    new_fit = zeros(size(fitness));
+    n = floor(elitism*pop_size);
+    
+    % Copy over the current best solutions to the new population
+    [f_sort, f_idx] = sort(fitness, 'descend');
+    pop_sort = population(f_idx,:);
+    new_pop(1:n,:) = pop_sort(1:n,:);
+    new_fit(1:n) = f_sort(1:n);
+    
+    % Build the roulette wheel from the fitnesses
+    wheel = zeros(1,pop_size);
+    for i = 1:pop_size
+        wheel(i) = sum(f_sort(1:i));
+    end
+    wheel = wheel/max(wheel);
+    
+    % Select chromosomes for the mating pool
+    for i = n+1:pop_size
+        x = rand();
+        index = find(wheel > x, 1);
+        new_pop(i,:) = pop_sort(index,:);
+        new_fit(i) = f_sort(index);
+    end
+    [new_fit, idx]  = sort(new_fit, 'descend');
+    new_pop = new_pop(idx, :);
+     %[new_pop(2:end,:), idx] = datasample(population, pop_size-1, 'Weights', fitness);
 end
