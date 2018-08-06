dc31c2336b2da2f794e08c99ed65e3392349a34e
Madeleine <mtownley@hotmail.ca>
2018-08-06 14:34:56 -0700

    increased adaptive mutation rate

--- mut.m
+++ mut.m
@@ -2,7 +2,7 @@ function result = mut(input)
     result = zeros(size(input));
     for j = 1:length(input)
         x = rand();
-        if x <= 0.002
+        if x <= 0.05
             result(j) = ~input(j);
         end
     end
