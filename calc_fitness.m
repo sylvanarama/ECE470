371ad1c8249eaaa902e0f15d725b231411d05575
Madeleine <mtownley@hotmail.ca>
2018-08-06 14:33:22 -0700

    Changed so fitness more closely resembles percent accuracy

--- calc_fitness.m
+++ calc_fitness.m
@@ -1,7 +1,11 @@
 % Calculate fitness from predictions
 
 function fitness = calc_fitness(predicted_labels, test_labels)
-    fitness =(abs(predicted_labels - test_labels)).^-1;
-    fitness(fitness == Inf) = 1;
-    fitness = sum(fitness)/length(test_labels);
+%     fitness =(abs(predicted_labels - test_labels)).^-1;
+%     fitness(fitness == Inf) = 1;
+%     fitness(fitness > 1) = 1;
+%     fitness = sum(fitness)/length(test_labels);
+    accuracy = 1-(abs(test_labels-predicted_labels)./test_labels)*100;
+    accuracy(accuracy < 0) = 0;
+    fitness = mean(accuracy);
 end
