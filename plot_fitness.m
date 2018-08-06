16718bce59ad65c842573724714fa7bb81dbfb2e
Madeleine <mtownley@hotmail.ca>
2018-08-06 14:29:39 -0700

    plot mean and max fitness after run

---
+++ plot_fitness.m
@@ -0,0 +1,12 @@
+figure(1)
+hold on
+g = 24;
+x = 1:g;
+%plot(results(1:g,1), 'Linewidth', 2)
+plot(results(1:g,2), 'Linewidth', 2)
+plot(results(1:g,3), 'Linewidth', 2)
+title('GA Performance over Generations')
+xlabel('Generation')
+ylabel('Fitness')
+%legend('Diversity', 'Mean Fitness', 'Max Fitness', 'Location', 'southeast')
+legend('Mean Fitness', 'Max Fitness', 'Location', 'southeast')
